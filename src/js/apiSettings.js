068cab46289aaad3e82b9cf67d7d2bbc6c3fb84b
mandrykk <86601388+mandrykk@users.noreply.github.com>
2021-12-05 16:54:19 +0200

    api function changes

--- src/js/apiSettings.js
+++ src/js/apiSettings.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '24658452-c9a73396c36aa22aa8e7e136d';
 
-export default function searchImages(userRequest, page) {
-  return axios.get(
+export default async function searchImages(userRequest, page) {
+  const response = await axios.get(
     `${API_URL}/?key=${API_KEY}&q=${userRequest}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
   );
+  return response
 }
 
